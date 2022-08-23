6caf62d524cfa6ab5a030b656cfe9486b1fee34f
DanilaZhebryakov <64162836+DanilaZhebryakov@users.noreply.github.com>
2022-08-23 15:50:51 +0300

    Change all

--- main.cpp
+++ main.cpp
@@ -1,39 +1,73 @@
 #include<stdio.h>
 #include<math.h>
-void solve_linear(float a,float b){
+#include<assert.h>
+const int EQ_anyroot = -1;
+
+int solveLinear(double a,double b,double* x){
+    assert(x);
+    assert(std::isfinite(a));
+    assert(std::isfinite(b));
     if(a == 0){
         if(b == 0){
-            printf("Solutions: any\n");
+            return EQ_anyroot;
         }
         else{
-            printf("No solutions\n");
+            return 0;
         }
     }
     else{
-        printf("Solution: %f\n", -b/a);
+        *x = -b/a;
+        return 1;
+    }
+}
+int solveSqare(double a,double b,double c,double* x1,double* x2){
+    assert(x1);
+    assert(x2);
+    assert(x1 != x2);
+    assert(std::isfinite(a));
+    assert(std::isfinite(b));
+    assert(std::isfinite(c));
+    if(a == 0){
+        return solveLinear(b,c,x1);
+    }
+    float discriminant = (b*b)-(4*a*c);
+    if(discriminant < 0){
+        return 0;
+    }
+    else if(discriminant == 0){
+        *x1 = -b/(2*a);
+        return 1;
+    }
+    else{
+        float sqrtdisc = sqrt(discriminant);
+        *x1 = (sqrtdisc-b)/(2*a);
+        *x2 = (-sqrtdisc-b)/(2*a);
+        return 2;
     }
 }
 int main()
 {
-    float a,b,c;
-    scanf("%f %f %f",&a,&b,&c);
-    if(a == 0){
-        printf("Linear equation\n");
-        solve_linear(b,c);
+    printf("Enter coeficients a,b,c for equation ax^2 + bx + c = 0\n");
+    double a=NAN,b=NAN,c=NAN,x1=NAN,x2=NAN;
+    scanf("%lf %lf %lf",&a,&b,&c);
+    if(!(std::isfinite(a) && std::isfinite(b) && std::isfinite(c))){
+        printf("Inputs should be three correct dechimal numbers\n");
         return 0;
     }
-    float discriminant = (b*b)-(4*a*c);
-    if(discriminant < 0){
-        printf("Equation has no solution\n");
-    }
-    else if(discriminant == 0){
-        printf("Equation has one solution\n");
-        printf("Solution: %f\n",-b/(2*a));
-    }
-    else{
-        printf("Equation has two solutions\n");
-        float sqrtdisc = sqrt(discriminant);
-        printf("Solutions: %f; %f\n",(sqrtdisc-b)/(2*a),(-sqrtdisc-b)/(2*a));
+    int nroots = solveSqare(a,b,c,&x1,&x2);
+    switch(nroots){
+    case 0:
+        printf("No solutions\n");
+        break;
+    case EQ_anyroot:
+        printf("Solution: any number\n");
+        break;
+    case 1:
+        printf("x = %lf\n",x1);
+        break;
+    case 2:
+        printf("x1 = %lf; x2 = %lf\n",x1,x2);
+        break;
     }
     return 0;
 }
