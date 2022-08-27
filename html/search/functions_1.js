367afcb87b7bcd062a69263268ca04577f60e08c
DanilaZhebryakov <64162836+DanilaZhebryakov@users.noreply.github.com>
2022-08-27 14:19:40 +0300

    update doxygened docs

--- html/search/functions_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['solvelinear_0',['solveLinear',['../equation_8cpp.html#a402a117e850635ac5c9c775c4d6da5c0',1,'solveLinear(double a, double b, double *const x):&#160;equation.cpp'],['../equation_8h.html#a402a117e850635ac5c9c775c4d6da5c0',1,'solveLinear(double a, double b, double *const x):&#160;equation.cpp']]],
-  ['solvesqare_1',['solveSqare',['../equation_8cpp.html#a73373241daabf0490a9357775322fae6',1,'solveSqare(double a, double b, double c, double *const x1, double *const x2):&#160;equation.cpp'],['../equation_8h.html#a73373241daabf0490a9357775322fae6',1,'solveSqare(double a, double b, double c, double *const x1, double *const x2):&#160;equation.cpp']]]
-];
--- html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['eq_5fanyroot_0',['EQ_anyroot',['../equation_8cpp.html#a289a78d5f1e42f1eac3748b8d61121bd',1,'EQ_anyroot():&#160;equation.cpp'],['../equation_8h.html#a289a78d5f1e42f1eac3748b8d61121bd',1,'EQ_anyroot():&#160;equation.h']]]
-];
